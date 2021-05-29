51befd7c4fe3e1057188eceac3c6c9dfa0cb7601
Lyndon Maydwell <lyndon@sordina.net>
2021-05-29 17:23:51 +1000

    draft queries

--- src/DBLights.hs
+++ src/DBLights.hs
@@ -1,6 +1,92 @@
 
 {-# LANGUAGE  BlockArguments #-}
 
+
+
+{-
+
+
+Remain alive:
+
+select l1.x, l1.y, count(*) from lights l1, lights as l2
+where
+  (l2.x <= l1.x + 1 and l2.x >= l1.x - 1)
+  and
+  (l2.y <= l1.y + 1 and l2.y >= l1.y - 1)
+  and
+  (l2.x <> l1.x or l2.y <> l1.y)
+
+group by l1.x, l1.y
+having count(*) >= 2 and count(*) <= 3
+
+
+Come Alive:
+
+-- create type pair AS (a int, b int);
+
+select c, (a::text::pair).a as x, (a::text::pair).b as y from
+(
+    select count(*) as c,
+        unnest(ARRAY[
+            (x-1, y-1), (x-1, y), (x-1, y+1),
+            (x,   y-1),           (x,   y+1),
+            (x+1, y-1), (x+1, y), (x+1, y+1)
+        ]) as a
+    from lights
+    group by a
+) as foo
+where a not in (select (x,y) from lights)
+and c = 3
+
+Unioned:
+
+-- create type pair AS (a int, b int);
+
+select c, (a::text::pair).a as x, (a::text::pair).b as y from
+(
+    select count(*) as c,
+        unnest(ARRAY[
+            (x-1, y-1), (x-1, y), (x-1, y+1),
+            (x,   y-1),           (x,   y+1),
+            (x+1, y-1), (x+1, y), (x+1, y+1)
+        ]) as a
+    from lights
+    group by a
+) as foo
+where a not in (select (x,y) from lights)
+and c = 3
+
+union
+
+select l1.x, l1.y, count(*) from lights l1, lights as l2
+where
+  (l2.x <= l1.x + 1 and l2.x >= l1.x - 1)
+  and
+  (l2.y <= l1.y + 1 and l2.y >= l1.y - 1)
+  and
+  (l2.x <> l1.x or l2.y <> l1.y)
+
+group by l1.x, l1.y
+having count(*) >= 2 and count(*) <= 3
+
+
+Other queries:
+
+
+query MyQuery($scheduled_time: String, $payload: jsonb, $name: String, $id:String) {
+  foo(where: {_and: [{test: {_neq: $scheduled_time}}, {test2: {_contains: $payload}}, {test: {_neq: $name}}, {test: {_neq: $id}}]}) {
+    id
+  }
+}
+
+{"scheduled_time": "asdf", "payload": {}, "name": "asdf", "id": "asdf"}
+
+
+
+-}
+
+
+
 import System.MIDI
     ( close,
       enumerateDestinations,
@@ -26,9 +112,13 @@ main = do
   mapM_ print pairs
   getArgs >>= setup pairs
 
+help :: IO ()
+help = putStrLn "Provide no arguments to pick first MIDI source, or specify a name."
+
 setup :: [(String, Destination)] -> [String] -> IO ()
 setup [] _                     = putStrLn "No MIDI destinations found."
-setup _  a | "--help" `elem` a = putStrLn "Provide no arguments to pick first MIDI source, or specify a name."
+setup _  a | "-h"     `elem` a = help
+setup _  a | "--help" `elem` a = help
 setup _  (_:_:_)               = putStrLn "Provide zero or one destination name as an argument."
 setup destinations []          = run (snd $ head destinations)
 setup destinations [n]         = do
@@ -40,23 +130,22 @@ run :: Destination -> IO ()
 run choice = do
   conn <- openDestination choice
   start conn
+  sequence_ [change conn x y 0 | x <- [0..7], y <- [0..7]]
   mapM_ (event conn) =<< (lines <$> getContents)
-  mapM_ (send conn . message 0) [0..64]
+  sequence_ [change conn x y 0 | x <- [0..7], y <- [0..7]]
   stop conn
   close conn
 
 event :: Connection -> String -> IO ()
 event d l = do
-  print [0..8]
   case map readMay (words l) of
-    [Just x, Just y, Just v] -> change d x y v
+    [Just x, Just y, Just v] -> print l >> change d x y v
     x -> putStrLn $ "Couldn't decode line: [" <> l <> "]. Expected format [x y v]. Example: [2 2 127]"
 
 change :: Connection -> Int -> Int -> Int -> IO ()
 change c x y v = do
   let z = y * 16 + x
-  mapM_ (send c . message 127) [z]
-  -- mapM_ (send c . message 127) [16..23]
+  mapM_ (send c . message v) [z]
 
 message :: Int -> Int -> MidiMessage
 message v p = MidiMessage 1 (NoteOn p v)
